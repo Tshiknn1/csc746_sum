906c940340606dd1274db89f246b0f68691334f7
Evan Robert Caplinger <tshiknn@login34.chn.perlmutter.nersc.gov>
2025-09-02 13:19:59 -0700

    First Perlmutter run complete

--- nersc_batch.sh
+++ nersc_batch.sh
@@ -5,7 +5,7 @@
 #SBATCH -J csc746_f25_evan_caplinger_sum
 #SBATCH --mail-user=ecaplinger@sfsu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 00:02:00
+#SBATCH -t 00:10:00
 
 #OpenMP settings:
 export OMP_NUM_THREADS=1
@@ -13,11 +13,11 @@ export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
 #run the application:
-srun -n 1 -c 256 --cpu_bind=cores ./sum_direct
-srun -n 1 -c 256 --cpu_bind=cores ./sum_vector
-srun -n 1 -c 256 --cpu_bind=cores ./sum_indirect
-srun -n 1 -c 256 --cpu_bind=cores ./sum_indirect
-srun -n 1 -c 256 --cpu_bind=cores ./sum_indirect
-srun -n 1 -c 256 --cpu_bind=cores ./sum_indirect_seed
-srun -n 1 -c 256 --cpu_bind=cores ./sum_indirect_seed
-srun -n 1 -c 256 --cpu_bind=cores ./sum_indirect_seed
+srun -n 1 -c 256 --cpu_bind=cores build/sum_direct
+srun -n 1 -c 256 --cpu_bind=cores build/sum_vector
+srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect
+srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect
+srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect
+srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect_seed
+srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect_seed
+srun -n 1 -c 256 --cpu_bind=cores build/sum_indirect_seed
